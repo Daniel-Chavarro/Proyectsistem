e76290d8e5c204ff238bff1dc3c55aafe0faea02
Daniel-Chavarro <108764157+Daniel-Chavarro@users.noreply.github.com>
2023-10-19 10:46:44 -0500

    Implementation of IA

---
+++ src_page/js/IAscript.js
@@ -0,0 +1,71 @@
+const webcamElement = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
+let net = null;
+
+// Check if webcam access is supported.
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it to call enableCam function which we will 
+// define in the next step.
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
+
+
+// Enable the live webcam view and start classification.
+function enableCam(event) {
+  
+  
+  
+  // Hide the button once clicked.
+  event.target.classList.add('removed');  
+  
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    webcamElement.srcObject = stream;
+    webcamElement.addEventListener('loadeddata', app());
+  });
+}
+
+
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
+
+  // Create an object from Tensorflow.js data API which could capture image
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+  while (true) {
+    const img = await webcam.capture();
+    const result = await net.classify(img);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+    // Dispose the tensor to release the memory.
+    img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
+}
+
