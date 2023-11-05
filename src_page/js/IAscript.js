21bdca47b2366bd806765b60d2cb763bc78c0a5b
Daniel-Chavarro <108764157+Daniel-Chavarro@users.noreply.github.com>
2023-11-05 15:02:10 -0500

    Code less dependient from modules

--- src_page/js/IAscript.js
+++ src_page/js/IAscript.js
@@ -1,61 +1,52 @@
-const button = document.getElementById("buttonStart")
-const text = document.getElementById("text")
-const screenWidthBp = matchMedia("(max-width: 556px)")
-var camWidth = 500 * 0.8
-var camHeight = 350 
+const video = document.createElement('video');
+const webcamContainer = document.getElementById('webcam-container');
+const labelContainer = document.getElementById('label-container');
+const buttonInit = document.getElementById('buttonStart')
+const URL = '../../src_IA/';
 
 
-// More API functions here:
-    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
-    // the link to your model provided by Teachable Machine export panel
-    const URL = "../../src_IA/";
 
-    let model, webcam, labelContainer, maxPredictions;
+function init() {
+    buttonInit.classList.add('disabled')
+    video.autoplay = true;
+    webcamContainer.appendChild(video);
 
-    // Load the image model and setup the webcam
-    async function init() {
-        
-        button.classList.add("disabled")
-        text.innerHTML = "Pon un objeto a la vista de la cámara."	
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
 
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+        video.srcObject = stream;
+        video.addEventListener('loadeddata', () => {
+        loadModel();
+        });
+    })
+    .catch((err) => {
+        console.error('Error accessing the webcam: ', err);
+    });
 
-        // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(camWidth, camHeight, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
-        await webcam.play();
-        window.requestAnimationFrame(loop);
-
-        // append elements to the DOM
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
-        labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
+    function loadModel() {
+    tmImage.load(URL + 'model.json', URL + 'metadata.json')
+        .then((model) => {
+        numTotalclasses = model.getTotalClasses();
+        for (let i = 0 ; i < numTotalclasses ; i++){
             labelContainer.appendChild(document.createElement("div"));
         }
-    }
+        predict(model,numTotalclasses);
+        console.log(model)
+        })
+        .catch((err) => {
+        console.error('Error loading the model: ', err);
+        });
 
-    async function loop() {
-        webcam.update(); // update the webcam frame
-        await predict();
-        window.requestAnimationFrame(loop);
-    }
-
-    // run the webcam image through the image model
-    async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction =
-                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
+    function predict(model, numTotalclasses) {
+        setInterval(async () => {   
+        const prediction = await model.predict(video);
+        
+        for (i = 0 ; i < numTotalclasses; i++){
+            const textPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2)
+            labelContainer.childNodes[i].innerHTML = textPrediction
         }
+        }, 1000);
     }
+    }
+}
